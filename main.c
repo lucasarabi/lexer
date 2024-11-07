b06195bc7206d09e2a3231bb8d3ad990571e7b13
lucasarabi <lucaarab01@gmail.com>
2024-11-07 15:51:59 -0500

    First commit; implemented fileIO

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define BUFF_SIZE 10000
+
+// Framework by which to read an input file in C for lexer project
+// Principles of Programming Languages
+//
+// Diego Angueira
+// Lucas Arabi
+//
+
+int main() {
+    // Open file and handle file read error
+    FILE *input = fopen("file_dir/input_file.txt", "r");
+    if(input == NULL) {
+        fprintf(stderr, "Error: File not found\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Line buffer    
+    char *line_buf = (char*)malloc(BUFF_SIZE);
+    
+    // Loop line by line
+    while(fgets(line_buf, BUFF_SIZE, input) != NULL) {
+        // REMOVE LATER
+        printf("%s", line_buf);
+     
+    }
+
+    return 0;
+}
